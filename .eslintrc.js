0e6548cfecca582c1e17556f3acaf2fadf9cc920
Richard <zfanli@outlook.com>
2022-05-14 15:00:46 +0800

    workspace setup

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    'vue/setup-compiler-macros': true,
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['vue'],
+  rules: {},
+}
